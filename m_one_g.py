f7e1a67ef42c5e92fba0abe67f1b2eb98433995e
Diego Gonzalez <djgh.117@gmail.com>
2020-08-11 20:27:00 -0400

    modified the script so that only runs a single simulation and saves snapshots every 100Myr

--- m_one_g.py
+++ m_one_g.py
@@ -5,6 +5,9 @@ from amuse.ext.galactics_model import new_galactics_model
 from prepare_figure import single_frame
 
 def make_plot(disk1, filename):
+    
+    # close any open plots
+    plt.close()
     x_label = "X [kpc]"
     y_label = "Y [kpc]"
     figure = single_frame(x_label, y_label, logy=False, xsize=14, ysize=14)
@@ -12,7 +15,7 @@ def make_plot(disk1, filename):
     pyplot.ylim(-300, 300)
 
     pyplot.scatter(disk1.x.value_in(units.kpc), disk1.y.value_in(units.kpc), 
-                   alpha=1, s=1, lw=0)
+                   alpha=1, s=3, lw=0)
 
     pyplot.savefig('plots/' + filename)
 
@@ -38,9 +41,23 @@ def simulate(galaxy1, converter, n_bulge, n_halo, t_end):
     disk1 = set1[:n_bulge]#:n_halo]
 
     make_plot(disk1, "g0myr")
-    dynamics.evolve_model(t_end)
-    make_plot(disk1,
-              "g"+str(t_end.value_in(units.Myr))+"myr")
+    
+    sentinel = 1
+    
+    # use a while loop to simulate in parts, to get
+    # snapshots of different parts of the simulation
+    while sentinel*100 <= t_end.value_in(units.Myr):
+        
+        # evolve another 100 Myr and save a plot
+        dynamics.evolve_model(100|units.Myr)
+        make_plot(disk1, "g"+str(100*sentinel)+"myr")
+        
+        # print to the terminal to see progress
+        print('Done with t = ' + str(100*sentinel) + ' Myr')
+        print()
+        
+        # add one to the sentinel
+        sentinel += 1
 
     dynamics.stop()
 
@@ -53,14 +70,14 @@ def new_option_parser():
     result.add_option("-R", unit=units.kpc,
                       dest="R_galaxy", default = 10 | units.kpc,
                       help="Galaxy size [%default]")
-    result.add_option("--n_bulge", dest="n_bulge", default = 30000,
+    result.add_option("--n_bulge", dest="n_bulge", default = 10000,
                       help="number of stars in the bulge [%default]")
     result.add_option("--n_disk", dest="n_disk", default = 30000,
                       help="number of stars in the disk [%default]")
     result.add_option("--n_halo", dest="n_halo", default = 60000,
                       help="number of stars in the halo [%default]")
     result.add_option("--t_end", unit=units.Myr,
-                      dest="t_end", default = 100|units.Myr,
+                      dest="t_end", default = 1000|units.Myr,
                       help="End of the simulation [%default]")
     return result
 
@@ -69,10 +86,13 @@ if __name__ == '__main__':
     galaxy1, converter = make_galaxy(o.M_galaxy, o.R_galaxy,
                                                 o.n_halo, o.n_bulge, o.n_disk)
     
+    simulate(galaxy1, converter, o.n_bulge, o.n_halo, o.t_end)
+    
+    """
     for i in numpy.arange(100,800,100):
         print()
         print('Starting simulation with t = ' + str(i) + 'Myr')
         simulate(galaxy1, converter, o.n_bulge, o.n_halo, i|units.Myr)
         print('Done with simulation with t = ' + str(i) + 'Myr')
-        print()
+        print()"""
 
